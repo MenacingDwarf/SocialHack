43557581ec5387a6d597704869343d2a8f7d4565
GrigorKhachatryan <grigor777001@yandex.ru>
2019-05-11 22:36:19 +0300

    add lesson

--- task/views.py
+++ task/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from database.models import Course, Lesson
+from database.models import Course, Lesson, Activity
 
 # Create your views here.
 def courses(request, id):
@@ -15,3 +15,9 @@ def update(request,id):
     obj = Lesson(title = lesson_name,course = Course.objects.get(id=id))
     obj.save()
     return redirect('/courses/{}'.format(id))
+
+def lesson(request, id_course, id_lesson):
+    lessons = Activity.objects.all().filter(lesson=id_lesson)
+
+
+    return render(request, 'task/lessons.html', {'lessons': lessons})

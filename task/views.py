020aee2b79b38347f2fc7d58386290ceba637f4f
Damir214 <damir211114@gmail.com>
2019-05-11 23:50:55 +0300

    Update views.py

--- task/views.py
+++ task/views.py
@@ -1,24 +1,20 @@
 from django.shortcuts import render,redirect
 from database.models import Course, Lesson, Activity
 
+
 # Create your views here.
 def courses(request, id):
     lessons = Lesson.objects.all().filter(course=id)
     teacher = Course.objects.get(id=id).tutor
-
-
     return render(request, 'task/lessons.html', {'lessons': lessons, 'id': id, 'teacher': teacher})
 
 def update(request,id):
     if request.method == 'POST':
         lesson_name = request.POST['lesson_name']
-
     obj = Lesson(title = lesson_name,course = Course.objects.get(id=id))
     obj.save()
     return redirect('/courses/{}'.format(id))
 
 def lesson(request, id_course, id_lesson):
     lessons = Activity.objects.all().filter(lesson=id_lesson)
-
-
     return render(request, 'task/lessons.html', {'lessons': lessons})

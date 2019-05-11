74bd14b864dffc4f11a2311bcc430c3be5c512e0
GrigorKhachatryan <grigor777001@yandex.ru>
2019-05-11 22:02:12 +0300

    create lessons

--- task/views.py
+++ task/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from database.models import Course, Lesson
 
 # Create your views here.
+def courses(request, id):
+    lessons = Lesson.objects.all().filter(course=id)
+
+
+    return render(request, 'task/lessons.html', {'lessons': lessons, 'id': id})
+
+def update(request,id):
+    if request.method == 'POST':
+        lesson_name = request.POST['lesson_name']
+
+    obj = Lesson(title = lesson_name,course = Course.objects.get(id=id))
+    obj.save()
+    return redirect('/courses/{}'.format(id))
